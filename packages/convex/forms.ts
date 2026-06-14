d23de843482b29d7977e897f7a7d6784ad028a2a
William Crutchfield <william.r.crutchfield@gmail.com>
2026-06-14 16:10:39 -0700

    implement create form

--- packages/convex/forms.ts
+++ packages/convex/forms.ts
@@ -1,6 +1,7 @@
+import { nano } from "@formbro/core/nanoid";
 import { fail, ok } from "@formbro/core/result";
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getWorkspaceAccess } from "./access";
 import { defineErrors } from "./errors";
 
@@ -8,6 +9,28 @@ export const ERRORS = defineErrors({
   FORM_NOT_FOUND: {
     message: "Form not found.",
     status: "NOT_FOUND",
+  },
+});
+
+export const create = mutation({
+  args: {
+    workspaceId: v.id("workspaces"),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const access = await getWorkspaceAccess(ctx, args.workspaceId);
+    if (!access.ok) return fail({ data: null, error: access.error });
+
+    let slug = nano();
+
+    await ctx.db.insert("forms", {
+      status: "draft",
+      slug,
+      workspaceId: args.workspaceId,
+      name: args.name,
+    });
+
+    return ok({ slug });
   },
 });
 
--- packages/web/app/(app)/dashboard/[workspace]/create-form-form.tsx
+++ packages/web/app/(app)/dashboard/[workspace]/create-form-form.tsx
@@ -1,18 +1,41 @@
 "use client";
 
+import { api } from "@formbro/convex/_generated/api";
 import { CREATE_FORM } from "@formbro/convex/system/forms/create_form";
 import { RiAddLine } from "@remixicon/react";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
 import * as React from "react";
 import { InternalDialogForm } from "@/components/internal-dialog-form";
+import { useRequiredWorkspaceData } from "./data-provider";
 
 export function CreateForm({ children }: { children?: React.ReactNode }) {
-  // const createForm = useMutation(api.core.forms.create);
+  const router = useRouter();
+  const { workspace } = useRequiredWorkspaceData();
+  const createForm = useMutation(api.forms.create);
+
   return (
     <InternalDialogForm
       title="Create Form"
       description="Create a new form in this workspace."
       schema={CREATE_FORM.typed}
       Icon={RiAddLine}
+      action={async ({ values }) => {
+        const form = await createForm({
+          workspaceId: workspace._id,
+          name: values.name ?? "",
+        });
+
+        if (form?.ok) {
+          router.prefetch(`/dashboard/${workspace.slug}/${form.data.slug}`);
+          console.log("prefetched", `/dashboard/${workspace.slug}/${form.data.slug}`);
+        }
+
+        return form;
+      }}
+      onSuccess={({ data }) => {
+        router.push(`/dashboard/${workspace.slug}/${data.slug}`);
+      }}
     >
       {children}
     </InternalDialogForm>
